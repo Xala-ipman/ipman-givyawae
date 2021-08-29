bbe873148f4bb9aae2e3919f811febd3a5de574d
Xala-ipman <89108413+Xala-ipman@users.noreply.github.com>
2021-08-30 00:49:22 +0300

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -14,7 +14,7 @@ module.exports.run = async (client, message, args) => {
       .addField("<a:emoji_7:881628868207857754> __Examples__ <a:emoji_7:881628868207857754>", "g+start #giveaway 5m 1 Testing\ng+end Testing\ng+reroll Testing")
       .addField("<a:emoji_9:881643825876054016>  __Utility__ <a:emoji_9:881643825876054016>", "ping, invite", true)
       .addField("<a:emoji_7:881628868207857754> ℹ Information ℹ <a:emoji_7:881628868207857754>", "stats", true)
-      .addField("Check out", "[Invite](https://discord.com/api/oauth2/authorize?client_id=838184020469743636&permissions=8&scope=bot) to make your own giveaway bot")
+      .addField("Check out", "[Invite](https://discord.com/api/oauth2/authorize?client_id=838184020469743636&permissions=8&scope=bot) invite Kurdish Giveaway in server / thank you")
       .setTimestamp()
       .setFooter(`Command Requested By ${message.author.tag}`, client.user.displayAvatarURL());
     message.channel.send("**Sent the commands in Direct Messages! <a:emoji_8:881643750017892352>, Check DMs**");
