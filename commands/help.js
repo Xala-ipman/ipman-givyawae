559b78d6acd405b35bdefe8659f7979d80d57320
Xala-ipman <89108413+Xala-ipman@users.noreply.github.com>
2021-08-30 00:30:58 +0300

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -11,7 +11,7 @@ module.exports.run = async (client, message, args) => {
       .setTitle("Command List & Guide for the Bot")
       .setDescription("Below are Commands you can do with Bot, Right now there is only 6 commands available, more commands will be added soon.")
       .addField("<a:emoji_5:881626214199078952> __Giveaway__ <a:emoji_5:881626214199078952>","start [channel-name] [Time] [winners] [Prize]\nreroll [prize name]\nend [prize name]")
-      .addField("<a:emoji_7:881628868207857754> __Examples__ <a:emoji_7:881628868207857754>", "g!start #giveaway 5m 1 Testing\ng!end Testing\ng!reroll Testing")
+      .addField("<a:emoji_7:881628868207857754> __Examples__ <a:emoji_7:881628868207857754>", "g+start #giveaway 5m 1 Testing\ng+end Testing\ng+reroll Testing")
       .addField("<a:emoji_9:881643825876054016>  __Utility__ <a:emoji_9:881643825876054016>", "ping, invite", true)
       .addField("<a:emoji_7:881628868207857754> ℹ Information ℹ <a:emoji_7:881628868207857754>", "stats", true)
       .addField("Check out", "[Invite](https://discord.com/api/oauth2/authorize?client_id=838184020469743636&permissions=8&scope=bot) to make your own giveaway bot")
